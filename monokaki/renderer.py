fece3b969219daeb4ef2b2a0c489101c012d706f
podhmo <ababjam61+github@gmail.com>
2018-09-24 19:06:54 +0900

    feat: renderer factory

--- monokaki/renderer.py
+++ monokaki/renderer.py
@@ -1,4 +1,5 @@
 import json
+from functools import partial
 
 
 class JSONRenderer:
@@ -12,3 +13,34 @@ class JSONRenderer:
 
 
 DEFAULT_RENDERER = JSONRenderer(sort_keys=True, ensure_ascii=False)
+
+
+def create_renderer_class(
+    fmt, style="%", dumps=partial(json.dumps, sort_keys=True, ensure_ascii=False)
+):
+    import logging
+    from collections import defaultdict
+
+    style_class, _ = logging._STYLES[style]
+    style = style_class(fmt)
+
+    class MockRecord:
+        pass
+
+    m = MockRecord()
+    idmap = defaultdict(lambda: len(idmap))
+    m.__dict__ = idmap
+    style.format(m)
+    names = [k for k, _ in sorted(idmap.items(), key=lambda p: p[1])]
+
+    class Renderer:
+        def __call__(self, d, record, formatter):
+            row = {}
+            formatter.format(record)
+            row["logger"] = record.name
+            for name in names:
+                if name not in row:
+                    row[name] = getattr(record, name, "")
+            return dumps(row)
+
+    return Renderer()
